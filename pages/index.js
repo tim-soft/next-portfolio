2a6ecdb2688beee6aee9149f2260472487a3ab1d
Tim Ellenberger <timellenberger@gmail.com>
2019-02-26 21:31:19 -0800

    Update head tags in index and portfolio pages

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,6 @@ export default () => (
   <>
     <Head>
       <title>Tim Ellenberger | Home</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
 
     <IntroBanner />
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import Head from 'next/head';
 import PageScrollWrapper from '../components/PageScrollWrapper';
 import PortfolioList from '../components/Portfolio';
 
 const PortfolioPage = () => (
-  <PageScrollWrapper>
-    <Title>Portfolio</Title>
-    <PortfolioList />
-  </PageScrollWrapper>
+  <>
+    <Head>
+      <title>Tim Ellenberger | Portfolio</title>
+    </Head>
+    <PageScrollWrapper>
+      <Title>Portfolio</Title>
+      <PortfolioList />
+    </PageScrollWrapper>
+  </>
 );
 
 // _app.js level theme variable overrides
 PortfolioPage.theme = {
   headerNavFontColor: 'black',
+  headerNavHoverFontColor: '#00e5e5',
   headerNavHamburgerIconColor: 'black'
 };
 
