09057e819fa5a42a0562e12ca258f611e1c96e0c
Tim Ellenberger <timellenberger@gmail.com>
2019-02-27 13:50:29 -0800

    Create separate service worker cache for images

--- next.config.js
+++ next.config.js
@@ -27,11 +27,26 @@ const nextConfig = {
   // Now 2.0 Build Type
   // https://nextjs.org/blog/next-8#serverless-nextjs
   target: 'serverless',
-  // Service Worker
+  // Service Worker implemented via next-offline and powered by Workbox
   // https://github.com/hanford/next-offline#next-offline-options
+  // https://developers.google.com/web/tools/workbox/guides/configure-workbox
   workboxOpts: {
     swDest: 'static/service-worker.js',
+    clientsClaim: true,
+    skipWaiting: true,
     runtimeCaching: [
+      // Always pull images from cache if available
+      {
+        urlPattern: /.*\.(?:png|jpg|jpeg|svg|gif|webp|ico)/,
+        handler: 'cacheFirst',
+        options: {
+          cacheName: 'image-cache',
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      },
+      // Cache all other secure content, but try to fetch from network first
       {
         urlPattern: /^https?.*/,
         handler: 'networkFirst',
