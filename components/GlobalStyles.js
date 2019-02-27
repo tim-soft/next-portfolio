0b604d0d10a25fa6c082d56309e1f10ad6c64df7
Tim Ellenberger <timellenberger@gmail.com>
2019-02-27 15:08:20 -0800

    comment

--- components/GlobalStyles.js
+++ components/GlobalStyles.js
@@ -1,7 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 import styledNormalize from 'styled-normalize';
 
-/* Adds global styles and normalize.css to the app */
+/**
+ * Adds global styles and normalize.css used in /pages/_app.js
+ *
+ * http://nicolasgallagher.com/about-normalize-css/
+ * https://www.styled-components.com/docs/api#createglobalstyle
+ */
 export default createGlobalStyle`
   ${styledNormalize}
   body {
