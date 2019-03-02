4c3d92256e18eddfe949979f1ba597b19dd0a24c
Tim Ellenberger <timellenberger@gmail.com>
2019-03-01 19:12:29 -0800

    Lightbox gesture tweaks

--- components/Lightbox/components/ImageStage/components/ImagePager/index.js
+++ components/Lightbox/components/ImageStage/components/ImagePager/index.js
@@ -5,11 +5,22 @@ import { useSprings, animated } from 'react-spring';
 import { useGesture } from 'react-with-gesture';
 import styled from 'styled-components';
 
-const ImagePager = ({ images, onClose }) => {
+const ImagePager = ({
+  images,
+  onClose,
+  currentIndex,
+  onClickPrev,
+  onClickNext
+}) => {
+  // Set initial image to show from list
   const index = useRef(0);
+  index.current = currentIndex;
 
+  // Creates a spring for each image, each with it's own config
+  // https://www.react-spring.io/docs/hooks/use-springs
   const [props, set] = useSprings(images.length, i => ({
-    x: i * window.innerWidth,
+    // Calculate the initial x coord for each image
+    x: (i - index.current) * window.innerWidth,
     sc: 1,
     display: 'block'
   }));
@@ -19,18 +30,33 @@ const ImagePager = ({ images, onClose }) => {
       ? Math.min(Math.max(num, clamp), higher)
       : Math.min(num, clamp);
 
-    // const currentIndex = index.current;
-
-    // if (currentIndex > nextIndex) onClickPrev();
-    // if (currentIndex < nextIndex) onClickNext();
-    // console.log({ currentIndex: index.current, nextIndex });
+    if (currentIndex > nextIndex) onClickPrev();
+    if (currentIndex < nextIndex) onClickNext();
 
     return nextIndex;
   };
 
+  // Animated image change from prop changes
+  set(i => {
+    // Hide image if not the current image
+    if (i < index.current - 1 && i > index.current + 1)
+      return { display: 'none' };
+
+    // Update current image position
+    const x = (i - index.current) * window.innerWidth;
+    const sc = 1;
+
+    // Config for current image spring
+    return { x, sc, display: 'block' };
+  });
+
+  // Bind mouse/touch gestures to component
+  // https://github.com/react-spring/react-with-gesture#api
   const bind = useGesture(
     ({ down, delta: [xDelta], direction: [xDir], distance, cancel }) => {
-      if (down && distance > window.innerWidth / 3)
+      // Cancel drag, pull back to current image
+      if (down && distance > window.innerWidth / 3) {
+        // Update current image index
         cancel(
           (index.current = clamp(
             index.current + (xDir > 0 ? -1 : 1),
@@ -38,15 +64,24 @@ const ImagePager = ({ images, onClose }) => {
             images.length - 1
           ))
         );
+      }
+
+      // Update spring config for image
       set(i => {
+        // Hide image if not the current image
         if (i < index.current - 1 && i > index.current + 1)
           return { display: 'none' };
+
+        // Update current image position
         const x = (i - index.current) * window.innerWidth + (down ? xDelta : 0);
         const sc = down ? 1 - distance / window.innerWidth / 3 : 1;
+
+        // Config for current image spring
         return { x, sc, display: 'block' };
       });
     }
   );
+
   return props.map(({ x, display, sc }, i) => (
     <animated.div
       {...bind()}
@@ -94,6 +129,7 @@ const ImagePager = ({ images, onClose }) => {
 };
 
 ImagePager.propTypes = {
+  currentIndex: PropTypes.number.isRequired,
   images: PropTypes.arrayOf(
     PropTypes.shape({
       src: PropTypes.string.isRequired,
