9d3f9b5b3fa31bf623c0cdf7f5fba5ac4b7af690
Tim Ellenberger <timellenberger@gmail.com>
2019-03-02 16:58:07 -0800

    Lightbox: Rewrite ImagePager with render props pattern, lift state

--- components/Lightbox/components/ImageStage/components/ImagePager/index.js
+++ components/Lightbox/components/ImageStage/components/ImagePager/index.js
@@ -1,145 +1,128 @@
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable no-shadow */
-import React, { useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { useSprings, animated } from 'react-spring';
-import { useGesture } from 'react-with-gesture';
 import styled from 'styled-components';
+import { Gesture } from 'react-with-gesture';
+import { Spring, animated } from 'react-spring/renderprops.cjs';
 
-const ImagePager = ({
-  images,
-  onClose,
-  currentIndex,
-  onClickPrev,
-  onClickNext
-}) => {
-  // Set initial image to show from list
-  const index = useRef(0);
-  index.current = currentIndex;
+class ImagePager extends React.Component {
+  static propTypes = {
+    onClose: PropTypes.func.isRequired,
+    onClickPrev: PropTypes.func.isRequired,
+    onClickNext: PropTypes.func.isRequired,
+    currentIndex: PropTypes.number.isRequired,
+    images: PropTypes.arrayOf(
+      PropTypes.shape({
+        src: PropTypes.string.isRequired,
+        caption: PropTypes.string.isRequired,
+        alt: PropTypes.string.isRequired,
+        width: PropTypes.number,
+        height: PropTypes.number
+      })
+    ).isRequired
+  };
 
-  // Creates a spring for each image, each with it's own config
-  // https://www.react-spring.io/docs/hooks/use-springs
-  const [props, set] = useSprings(images.length, i => ({
-    // Calculate the initial x coord for each image
-    x: (i - index.current) * window.innerWidth,
-    sc: 1,
-    display: 'block'
-  }));
+  // Determine whether to slide to next or previous image
+  clamp = (num, clamp, higher) => {
+    const { currentIndex, onClickPrev, onClickNext } = this.props;
 
-  const clamp = (num, clamp, higher) => {
     const nextIndex = higher
       ? Math.min(Math.max(num, clamp), higher)
       : Math.min(num, clamp);
 
     if (currentIndex > nextIndex) onClickPrev();
-    if (currentIndex < nextIndex) onClickNext();
-
-    return nextIndex;
+    else if (currentIndex < nextIndex) onClickNext();
   };
 
-  // Animated image change from prop changes
-  set(i => {
-    // Hide image if not the current image
-    if (i < index.current - 1 && i > index.current + 1)
-      return { display: 'none' };
+  render() {
+    const { images, currentIndex, onClose } = this.props;
 
-    // Update current image position
-    const x = (i - index.current) * window.innerWidth;
-    const sc = 1;
+    return images.map((image, i) => (
+      <Gesture key={i}>
+        {({ down, delta: [xDelta], direction: [xDir], distance, cancel }) => {
+          // Animate over to the next image if it has been dragged more than 33% of screen width
+          if (currentIndex === i && down && distance > window.innerWidth / 3) {
+            cancel(
+              this.clamp(
+                currentIndex + (xDir > 0 ? -1 : 1),
+                0,
+                images.length - 1
+              )
+            );
+          }
 
-    // Config for current image spring
-    return { x, sc, display: 'block' };
-  });
+          let gestureConfig = {};
 
-  // Bind mouse/touch gestures to component
-  // https://github.com/react-spring/react-with-gesture#api
-  const bind = useGesture(
-    ({ down, delta: [xDelta], direction: [xDir], distance, cancel }) => {
-      // Cancel drag, pull back to current image
-      if (down && distance > window.innerWidth / 3) {
-        // Update current image index
-        cancel(
-          (index.current = clamp(
-            index.current + (xDir > 0 ? -1 : 1),
-            0,
-            images.length - 1
-          ))
-        );
-      }
+          // Hide image if not the current image
+          if (i < currentIndex - 1 && i > currentIndex + 1)
+            gestureConfig = { display: 'none' };
+          else {
+            // Update current image position
+            const x =
+              (i - currentIndex) * window.innerWidth + (down ? xDelta : 0);
+            const sc = down ? 1 - distance / window.innerWidth / 3 : 1;
 
-      // Update spring config for image
-      set(i => {
-        // Hide image if not the current image
-        if (i < index.current - 1 && i > index.current + 1)
-          return { display: 'none' };
+            gestureConfig = { x, sc, display: 'block' };
+          }
 
-        // Update current image position
-        const x = (i - index.current) * window.innerWidth + (down ? xDelta : 0);
-        const sc = down ? 1 - distance / window.innerWidth / 3 : 1;
-
-        // Config for current image spring
-        return { x, sc, display: 'block' };
-      });
-    }
-  );
-
-  return props.map(({ x, display, sc }, i) => (
-    <animated.div
-      {...bind()}
-      // eslint-disable-next-line react/no-array-index-key
-      key={i}
-      style={{
-        display,
-        transform: x.interpolate(x => `translate3d(${x}px,0,0)`),
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        willChange: 'transform',
-        userSelect: 'none'
-      }}
-    >
-      <animated.div
-        style={{
-          transform: sc.interpolate(s => `scale(${s})`),
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          willChange: 'transform',
-          userSelect: 'none'
+          return (
+            <Spring
+              native
+              to={{
+                x: (i - currentIndex) * window.innerWidth,
+                sc: 1,
+                display: 'block',
+                ...gestureConfig
+              }}
+            >
+              {({ x, display, sc }) => (
+                <animated.div
+                  style={{
+                    display,
+                    transform: x.interpolate(x => `translate3d(${x}px,0,0)`),
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                    willChange: 'transform',
+                    userSelect: 'none'
+                  }}
+                >
+                  <animated.div
+                    style={{
+                      transform: sc.interpolate(s => `scale(${s})`),
+                      width: '100%',
+                      height: '100%',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      willChange: 'transform',
+                      userSelect: 'none'
+                    }}
+                    // If the background is clicked close the lightbox
+                    onClick={() => {
+                      if (i === currentIndex && xDelta === 0) onClose();
+                    }}
+                  >
+                    <Image
+                      src={image.src}
+                      alt={image.alt}
+                      draggable="false"
+                      onClick={e => {
+                        e.stopPropagation();
+                        e.nativeEvent.stopImmediatePropagation();
+                      }}
+                    />
+                  </animated.div>
+                </animated.div>
+              )}
+            </Spring>
+          );
         }}
-        onClick={() => {
-          // If the background is clicked and a drag isn't happening
-          // close the lightbox
-          if (sc.getValue() === 1) onClose();
-        }}
-      >
-        <Image
-          src={images[i].src}
-          alt={images[i].alt}
-          draggable="false"
-          onClick={e => {
-            e.stopPropagation();
-            e.nativeEvent.stopImmediatePropagation();
-          }}
-        />
-      </animated.div>
-    </animated.div>
-  ));
-};
-
-ImagePager.propTypes = {
-  currentIndex: PropTypes.number.isRequired,
-  images: PropTypes.arrayOf(
-    PropTypes.shape({
-      src: PropTypes.string.isRequired,
-      caption: PropTypes.string.isRequired,
-      alt: PropTypes.string.isRequired,
-      width: PropTypes.number,
-      height: PropTypes.number
-    })
-  ).isRequired
-};
+      </Gesture>
+    ));
+  }
+}
 
 export default ImagePager;
 
