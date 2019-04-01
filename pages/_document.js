15490fd2b39a0478890aed1227a73d18b3905ec5
Tim Ellenberger <timellenberger@gmail.com>
2019-03-31 17:14:31 -0700

    Update icon tags in _document

--- pages/_document.js
+++ pages/_document.js
@@ -38,6 +38,23 @@ export default class StyledDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicon-16x16.png"
+          />
           <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
           <link rel="manifest" href="/static/manifest.json" />
           <meta
