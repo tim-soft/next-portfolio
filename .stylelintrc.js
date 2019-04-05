d7e85a3a28a6d8b5f98100652a06f8fcb3c8d4bc
Tim Ellenberger <timellenberger@gmail.com>
2019-04-04 19:02:59 -0700

    Implement StyleLint for Styled-Components

---
+++ .stylelintrc.js
@@ -0,0 +1,12 @@
+/**
+ * Configure Stylint to work with Styled-Components
+ *
+ * https://www.styled-components.com/docs/tooling#stylelint
+ */
+module.exports = {
+  processors: ['stylelint-processor-styled-components'],
+  extends: [
+    'stylelint-config-recommended',
+    'stylelint-config-styled-components'
+  ]
+};
