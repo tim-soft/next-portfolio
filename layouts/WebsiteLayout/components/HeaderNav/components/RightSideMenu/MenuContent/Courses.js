11ee579e971004d22ffaedbf40e88efa37bcbc84
Tim Ellenberger <timellenberger@gmail.com>
2019-04-06 18:31:55 -0700

    Add course logos to static dir

--- layouts/WebsiteLayout/components/HeaderNav/components/RightSideMenu/MenuContent/Courses.js
+++ layouts/WebsiteLayout/components/HeaderNav/components/RightSideMenu/MenuContent/Courses.js
@@ -5,31 +5,31 @@ const data = [
   {
     title: 'React',
     description: 'Coming Soon...',
-    logoSrc: 'https://cdn.svgporn.com/logos/react.svg',
+    logoSrc: '/static/course-logos/react.svg',
     logoAlt: 'React'
   },
   {
     title: 'GraphQL',
     description: 'Coming Soon...',
-    logoSrc: 'https://cdn.svgporn.com/logos/graphql.svg',
+    logoSrc: '/static/course-logos/graphql.svg',
     logoAlt: 'GraphQL'
   },
   {
     title: 'Next.js',
     description: 'Coming Soon...',
-    logoSrc: 'https://cdn.svgporn.com/logos/nextjs.svg',
+    logoSrc: '/static/course-logos/nextjs.svg',
     logoAlt: 'Next.js'
   },
   {
     title: 'Publishing to NPM',
     description: 'Coming Soon...',
-    logoSrc: 'https://cdn.svgporn.com/logos/npm.svg',
+    logoSrc: '/static/course-logos/npm.svg',
     logoAlt: 'NPM'
   },
   {
     title: 'JavaScript ES6+',
     description: 'Coming Soon...',
-    logoSrc: 'https://cdn.svgporn.com/logos/javascript.svg',
+    logoSrc: '/static/course-logos/javascript.svg',
     logoAlt: 'JavaScript'
   }
 ];
