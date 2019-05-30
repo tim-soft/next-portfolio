eb982b05518682539408dd3d751ecaaef9855fcc
Tim Ellenberger <timellenberger@gmail.com>
2019-05-30 15:52:14 -0700

    wip fix horizontal scrolling of code block

--- components/Blog/BlogCodeBlock/index.js
+++ components/Blog/BlogCodeBlock/index.js
@@ -5,25 +5,27 @@ import Highlight, { defaultProps } from 'prism-react-renderer';
 import theme from 'prism-react-renderer/themes/vsDark';
 
 const BlogCodeBlock = ({ code, language }) => (
-  <Highlight
-    {...defaultProps}
-    theme={theme}
-    code={code.trim()}
-    language={language}
-  >
-    {({ className, style, tokens, getLineProps, getTokenProps }) => (
-      <Pre className={className} style={style}>
-        {tokens.map((line, i) => (
-          <div {...getLineProps({ line, key: i })}>
-            <LineNumber>{i + 1}</LineNumber>
-            {line.map((token, key) => (
-              <span {...getTokenProps({ token, key })} />
-            ))}
-          </div>
-        ))}
-      </Pre>
-    )}
-  </Highlight>
+  <ScrollingContainer>
+    <Highlight
+      {...defaultProps}
+      theme={theme}
+      code={code.trim()}
+      language={language}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <Pre className={className} style={style}>
+          {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: i })}>
+              <LineNumber>{i + 1}</LineNumber>
+              {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </Pre>
+      )}
+    </Highlight>
+  </ScrollingContainer>
 );
 
 BlogCodeBlock.propTypes = {
@@ -41,9 +43,18 @@ BlogCodeBlock.defaultProps = {
 
 export default BlogCodeBlock;
 
+const ScrollingContainer = styled.div`
+  width: auto;
+  max-width: calc(100vw - 40px);
+  overflow: hidden;
+  overflow-x: auto;
+  background: #1e1e1e;
+  margin: 2em 0;
+`;
+
 const Pre = styled.pre`
   text-align: left;
-  margin: 2em 0;
+  /* margin: 2em 0; */
   padding: 0.5em 1em;
   font-size: 1.05em;
   cursor: text;
