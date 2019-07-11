2b7b64b6a681700a5e8c712c69b3a3a20ea0ff25
Tim Ellenberger <timellenberger@gmail.com>
2019-07-11 04:52:24 -0700

    Generate sitemap and robots.txt from /pages

---
+++ tools/generate-sitemap.js
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import getPathsObject from './getPathsObject';
+
+// Create public folder
+if (!fs.existsSync('./public')) {
+  fs.mkdirSync('./public');
+}
+
+// ROBOTS.txt
+const robotsTxt = `User-agent: *
+Sitemap: https://timellenberger.now.sh/sitemap.xml
+Disallow:`;
+
+fs.writeFileSync('public/robots.txt', robotsTxt);
+
+// SITEMAP.XML
+const pathsObj = getPathsObject();
+const today = new Date().toISOString();
+const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
+  ${Object.keys(pathsObj).map(
+    path => `<url>
+    <loc>https://timellenberger.now.sh${path}</loc>
+    <lastmod>${
+      pathsObj[path].lastModified
+        ? new Date(pathsObj[path].lastModified).toISOString()
+        : today
+    }</lastmod>
+  </url>`
+  )}
+</urlset>`;
+
+fs.writeFileSync('public/sitemap.xml', sitemapXml);
