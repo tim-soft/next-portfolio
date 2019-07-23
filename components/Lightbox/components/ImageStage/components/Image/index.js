f167f5b7942a76db7bfe093c9db0d325097db4e7
Tim Ellenberger <timellenberger@gmail.com>
2019-07-23 00:11:42 -0700

    Use spring animation for zoom, add will-change for much better performance

--- components/Lightbox/components/ImageStage/components/Image/index.js
+++ components/Lightbox/components/ImageStage/components/Image/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { useSpring, animated } from 'react-spring';
@@ -11,39 +11,30 @@ import { useGesture } from 'react-use-gesture';
  * https://github.com/react-spring/react-spring
  */
 const Image = ({ src, alt, isCurrentImage, toggleControls }) => {
-  const [scale, setScale] = useState(1);
-  const [immediate, setImmediate] = useState(false);
+  const [{ scale }, set] = useSpring(() => ({ scale: 1 }));
 
   // Reset scale of this image when switching to new image
   useEffect(() => {
-    if (!isCurrentImage) {
-      setImmediate(false);
-      setScale(1);
-    } else {
-      setImmediate(true);
-    }
-  });
-
-  const animStyles = useSpring({
-    immediate,
-    to: { transform: `scale(${scale})` }
+    if (!isCurrentImage) set({ scale: 1 });
   });
 
   // Animate current page and adjacent pages during drag
   const bind = useGesture({
     onPinch: ({ delta: [deltaDist] }) => {
-      const pinchScale = scale + deltaDist * 0.0004;
+      const pinchScale = scale.value + deltaDist * 0.004;
 
-      if (pinchScale < 0.5) setScale(0.5);
-      else if (pinchScale > 3.0) setScale(3.0);
-      else setScale(pinchScale);
+      if (pinchScale < 0.5) set({ scale: 0.5 });
+      else if (pinchScale > 3.0) set({ scale: 3.0 });
+      else set({ scale: pinchScale });
     }
   });
 
   return (
     <AnimatedImage
       {...bind()}
-      style={animStyles}
+      style={{
+        transform: scale.to(s => `scale(${s})`)
+      }}
       src={src}
       alt={alt}
       draggable="false"
@@ -77,6 +68,7 @@ const AnimatedImage = animated(styled.img`
   max-height: 100%;
   max-width: 100%;
   user-select: none;
+  will-change: transform;
   filter: drop-shadow(0 25px 50px rgba(0, 0, 0, 0.4))
     drop-shadow(0 0 2px rgba(0, 0, 0, 0.5));
 `);
