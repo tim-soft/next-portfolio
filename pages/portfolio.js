85d916c21d19f6a8c67be3bfac5540d8e5cc746f
Tim Ellenberger <timellenberger@gmail.com>
2019-08-14 15:45:05 -0700

    Remove placeholder content on porfolio page, add open source projects

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import styled, { ThemeProvider } from 'styled-components';
 import NextSEO from 'next-seo';
 import PageScrollWrapper from 'components/PageScrollWrapper';
-import PortfolioList from 'components/PortfolioList';
 
 const PortfolioPage = ({ theme }) => (
   <>
@@ -17,8 +16,71 @@ const PortfolioPage = ({ theme }) => (
     />
     <ThemeProvider theme={theme}>
       <PageScrollWrapper>
-        <Title>Portfolio</Title>
-        <PortfolioList />
+        <PageContainer>
+          <Title>Portfolio</Title>
+          <section>
+            <h2>Open Source</h2>
+            <p>
+              Aside from numerous contributions to <i>other</i> open-source
+              projects such as ant-design, react-starter-kit, react-use-gesture,
+              graphql-tools and others, I&apos;ve also released several projects
+              of my own.
+            </p>
+            <h3>
+              <a
+                href="https://github.com/tim-soft/next-portfolio"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                next-portfolio
+              </a>
+            </h3>
+            <p>
+              My personal website, blog and library demo playground, built from
+              scratch with Next.js
+            </p>
+            <h3>
+              <a
+                href="https://github.com/tim-soft/react-particles-webgl"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                react-particles-webgl
+              </a>
+            </h3>
+            <p>A 2D/3D particle library built on React, Three.js and WebGL</p>
+            <h3>
+              <a
+                href="https://github.com/tim-soft/react-spring-lightbox"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                react-spring-lightbox
+              </a>
+            </h3>
+            <p>
+              A flexible image gallery lightbox with native-feeling touch
+              gestures and buttery smooth animations, built with react-spring.
+            </p>
+            <h3>
+              <a
+                href="https://github.com/tim-soft/use-double-click"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                use-double-click
+              </a>
+            </h3>
+            <p>
+              A simple React hook for differentiating single and double clicks
+              on the same component.
+            </p>
+          </section>
+          <section>
+            <h2>Products</h2>
+            <p>[coming soon]</p>
+          </section>
+        </PageContainer>
       </PageScrollWrapper>
     </ThemeProvider>
   </>
@@ -34,7 +96,23 @@ PortfolioPage.defaultProps = {
 
 export default PortfolioPage;
 
+const PageContainer = styled.main`
+  width: 100%;
+  max-width: 1100px;
+  > section {
+    margin-bottom: 4em;
+  }
+  a {
+    transition: color 0.2s linear;
+    color: #0905d2;
+    :hover {
+      color: ${({ theme }) => theme.pageContentLinkHoverColor};
+    }
+  }
+`;
+
 const Title = styled.h1`
-  color: black;
+  color: ${({ theme }) => theme.pageContentFontColor};
   font-size: 50px;
+  text-align: center;
 `;
