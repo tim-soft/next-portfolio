972def4df71fdb9af2681f75cc282fa5ce44777c
Tim Ellenberger <timellenberger@gmail.com>
2019-08-26 10:52:02 -0700

    Remove deprecated <Container /> from _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import Router from 'next/router';
 import { version } from 'next/package.json';
 import NextSeo from 'next-seo';
@@ -95,7 +95,7 @@ class WebApp extends App {
     ];
 
     return (
-      <Container>
+      <>
         <NextSeo config={defaultSEO} />
         <GlobalStyles />
         <ThemeProvider theme={theme}>
@@ -111,17 +111,17 @@ class WebApp extends App {
               enter={{ opacity: 1, transform: 'scale(1) translateY(0px)' }}
               leave={{ opacity: 0, transform: 'scale(0.9) translateY(-200px)' }}
             >
-              {page => animStyles => (
-                <AnimatedContainer key={page.route} style={animStyles}>
-                  <page.Component
-                    {...page.pageProps}
+              {() => animStyles => (
+                <AnimatedContainer key={router.route} style={animStyles}>
+                  <Component
+                    {...pageProps}
                     // Pass route from transition data
-                    route={page.route}
+                    route={router.route}
                     // Combine page theme from transition with current dynamic page theme
                     // for this route
                     theme={{
-                      ...page.pageTheme,
-                      ...this.getDynamicPageTheme(page.route)
+                      ...pageTheme,
+                      ...this.getDynamicPageTheme(router.route)
                     }}
                     updateTheme={this.updateTheme}
                   />
@@ -130,7 +130,7 @@ class WebApp extends App {
             </Transition>
           </WebsiteLayout>
         </ThemeProvider>
-      </Container>
+      </>
     );
   }
 }
