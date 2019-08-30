bbd81064f065a73bfc499228ad357c7ab0e503a0
Tim Ellenberger <timellenberger@gmail.com>
2019-08-30 14:21:49 -0700

    Add libraries data

---
+++ data/Libraries.js
@@ -0,0 +1,96 @@
+export default [
+  {
+    name: 'react-particles-webgl',
+    href: '/libraries/react-particles-webgl',
+    description: 'A 2D/3D particle library built on React, Three.js and WebGL',
+    logoSrc: '/static/avatar.png',
+    badges: [
+      {
+        badgeUrl:
+          'https://img.shields.io/npm/v/react-particles-webgl.svg?color=brightgreen&style=popout-square',
+        linkUrl: 'https://www.npmjs.com/package/react-particles-webgl'
+      },
+      {
+        badgeUrl:
+          'https://img.shields.io/github/license/tim-soft/react-particles-webgl?color=brightgreen&style=popout-square',
+        linkUrl:
+          'https://github.com/tim-soft/react-particles-webgl/blob/master/LICENSE.md'
+      },
+      {
+        badgeUrl:
+          'https://img.shields.io/bundlephobia/minzip/react-particles-webgl.svg?style=popout-square',
+        linkUrl: 'https://bundlephobia.com/result?p=react-particles-webgl'
+      },
+      {
+        badgeUrl:
+          'https://img.shields.io/travis/tim-soft/react-particles-webgl?style=flat-square',
+        linkUrl: 'https://travis-ci.org/tim-soft/react-particles-webgl'
+      }
+    ]
+  },
+  {
+    name: 'react-spring-lightbox',
+    href: '/libraries/react-spring-lightbox',
+    description:
+      'A flexible image gallery lightbox with native-feeling touch gestures and buttery smooth animations, built with react-spring.',
+    logoSrc: '/static/avatar.png',
+    badges: [
+      {
+        badgeUrl:
+          'https://img.shields.io/npm/v/react-spring-lightbox.svg?color=brightgreen&style=popout-square',
+        linkUrl: 'https://www.npmjs.com/package/react-spring-lightbox'
+      },
+      {
+        badgeUrl:
+          'https://img.shields.io/github/license/tim-soft/react-spring-lightbox?color=brightgreen&style=popout-square',
+        linkUrl:
+          'https://github.com/tim-soft/react-spring-lightbox/blob/master/LICENSE.md'
+      },
+      {
+        badgeUrl:
+          'https://img.shields.io/bundlephobia/minzip/react-spring-lightbox.svg?style=popout-square',
+        linkUrl: 'https://bundlephobia.com/result?p=react-spring-lightbox'
+      },
+      {
+        badgeUrl:
+          'https://img.shields.io/travis/tim-soft/react-spring-lightbox?style=flat-square',
+        linkUrl: 'https://travis-ci.org/tim-soft/react-spring-lightbox'
+      }
+    ]
+  },
+  {
+    name: 'use-double-click',
+    href: 'https://github.com/tim-soft/use-double-click',
+    description:
+      'A simple React hook for differentiating single and double clicks on the same component.',
+    logoSrc: '/static/avatar.png',
+    badges: [
+      {
+        badgeUrl:
+          'https://img.shields.io/npm/v/use-double-click.svg?color=brightgreen&style=popout-square',
+        linkUrl: 'https://www.npmjs.com/package/use-double-click'
+      },
+      {
+        badgeUrl:
+          'https://img.shields.io/github/license/tim-soft/use-double-click?color=brightgreen&style=popout-square',
+        linkUrl:
+          'https://github.com/tim-soft/use-double-click/blob/master/LICENSE.md'
+      },
+      {
+        badgeUrl:
+          'https://img.shields.io/bundlephobia/minzip/use-double-click.svg?style=popout-square',
+        linkUrl: 'https://bundlephobia.com/result?p=use-double-click'
+      },
+      {
+        badgeUrl:
+          'https://img.shields.io/codecov/c/github/tim-soft/use-double-click?style=flat-square',
+        linkUrl: 'https://codecov.io/gh/tim-soft/use-double-click'
+      },
+      {
+        badgeUrl:
+          'https://img.shields.io/travis/tim-soft/use-double-click?style=flat-square',
+        linkUrl: 'https://travis-ci.org/tim-soft/use-double-click'
+      }
+    ]
+  }
+];
