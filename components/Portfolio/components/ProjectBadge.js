497bf4d23be04dfb1c440a533b92cfa69e71b990
Tim Ellenberger <timellenberger@gmail.com>
2019-08-30 14:22:55 -0700

    BadgeLink: add styles and prevent click propagation

--- components/Portfolio/components/ProjectBadge.js
+++ components/Portfolio/components/ProjectBadge.js
@@ -2,7 +2,12 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const ProjectBadge = ({ badgeUrl, linkUrl }) => (
-  <BadgeLink href={linkUrl} target="_blank" rel="noopener noreferrer">
+  <BadgeLink
+    onClick={e => e.stopPropagation()}
+    href={linkUrl}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
     <img src={badgeUrl} alt="badge" height="100%" />
   </BadgeLink>
 );
@@ -15,7 +20,14 @@ ProjectBadge.propTypes = {
 export default ProjectBadge;
 
 const BadgeLink = styled.a`
-  margin-right: 5px;
+  transition: border-color 0.2s linear !important;
+  margin-right: 3px;
   display: inline-block;
   height: 23px;
+  border-style: solid;
+  border-width: 1px;
+  border-color: transparent;
+  :hover {
+    border-color: ${({ theme }) => theme.pageContentLinkHoverColor};
+  }
 `;
