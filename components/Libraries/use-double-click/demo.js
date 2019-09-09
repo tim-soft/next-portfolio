f5259d4e066a2a2a16c74dfb678d9f7bcf58890b
Tim Ellenberger <timellenberger@gmail.com>
2019-09-08 18:11:57 -0700

    Adjust demo button font size

--- components/Libraries/use-double-click/demo.js
+++ components/Libraries/use-double-click/demo.js
@@ -74,18 +74,14 @@ const DemoContainer = styled.div`
 const StyledButton = styled.button`
   transition: all 0.2s linear;
   transition-property: border-color, background-color, color;
-
   color: ${({ theme }) => theme.pageContentFontColor};
-
   background-color: ${({ theme }) => theme.accentColor};
   border-color: ${({ theme }) => theme.popoutMenuBorderColor};
   border-width: 2px;
   border-style: solid;
   border-radius: 5px;
-
   line-height: 2.5em;
-  font-size: 17px;
-
+  font-size: 1.2em;
   padding: 0 10px;
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
   :focus {
